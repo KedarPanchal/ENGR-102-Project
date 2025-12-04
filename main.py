b34579d7e3c069fac046fa4a8d8dfa07a97bdc12
KedarPanchal <kedar.t.panchal@outlook.com>
2025-12-04 16:22:05 -0600

    Clear redundant code

--- main.py
+++ main.py
@@ -7,38 +7,3 @@ def displayrules():
     rules = open('Flip7Rules.txt', 'r')
     rules_text = rules.read()
     print(rules_text)
-
-
-def startround(players):
-    '''Each player decides if they want to hit or stay and runs through the round'''
-    listofplayers = []
-    for i in range(players):
-        listofplayers += Player(i)
-    currentplayernum = 1
-    game = True
-    while (game):
-        currentplayer = listofplayers[currentplayernum - 1]
-        choice = input(f"Player {currentplayernum}, would you like to hit or pass?")
-        if (choice.lower() == "hit"):
-            currentplayer.hit()
-        elif (choice.lower() == "pass"):
-            currentplayer.stay()
-
-        if currentplayer.has_seven() or currentplayer.is_busted():
-            game = False
-
-        currentplayernum += 1
-        if (currentplayernum > players):
-            currentplayernum = 1
-
-
-def calculatescore():
-    pass
-
-
-displayrules()
-
-# Make sure the player count is within 3-18
-player_count = int(input("Enter the number of players: "))
-while player_count < 3 or player_count > 18:
-    player_count = int(input("Invalid player count (must be 3-18). Enter new player count: "))
